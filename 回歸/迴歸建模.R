aface3fcd9f1a1e773590d9e4b5a95ce12ccd64c
LAPTOP-U59K2599\ASUS <kyle20030819@gmail.com>
2023-12-26 23:20:11 +0800

    Phase IV

--- 回歸/迴歸建模.R
+++ 回歸/迴歸建模.R
@@ -35,3 +35,53 @@ Sindex=sample(nrow(STdata),70) #隨機抽取70筆要保留的部分資料
 #Sindex=sample(N, round(N*0.8)) #抽取80%作為要保留的部分資料
 Train=STdata[Sindex,]
 Test=STdata[-Sindex,] #把資料分為訓練資料跟測試資料
+
+#Phase IV : Diagnostic
+pairs(Stime~.,data=Train) #用散布圖檢查變數間的關係
+cor(Train[,c(2,3,4,5,6,8)]) #correlation 越高可能代表貢共線性越明顯
+
+M1=lm(Stime~.,data=Train)
+summary(M1)
+
+library(car)
+library(lmtest)
+library(nortest)
+library(randtests)
+###Function Form and Homogeneity
+#e=residuals(M1)
+es=rstandard(M1) #求出殘差
+yhat=fitted.values(M1)
+
+plot(yhat,es,col='2')
+#plot(yhat,es,col='2')
+abline(h=0) #e-yhat plot
+residualPlot(M1,type="rstandard",quadratic=F) 
+#畫出殘差圖，看資料的中心是否平穩在0的虛線上(function form);
+#以及資料變異的一致性，看點是否均勻(齊一性)
+
+resettest(M1,power=2,type='regressor') 
+#檢驗屬量變數的二次項是否同時為零
+#p value 小於0.05，拒絕了!代表此模型有潛在的二次趨勢
+
+ncvTest(M1)
+#Breusch-Pagan test
+#檢定齊一性的，看sigma i 是否不論i為和皆相同
+#拒絕了!代表sigma i之間存在顯著差異
+
+#Normality
+qqPlot(M1)
+lillie.test(es)
+#KS test for normality
+#H0 : Yi服從常態 H1: Yi不服從常態
+shapiro.test(es)
+#Shapiro-Wilk Normality Test
+#H0 : Yi服從常態 H1: Yi不服從常態
+
+plot(es,type = "l",col='2')
+acf(es, ci=0.99)
+#dwtest(M1)#Durbin-Watson test
+
+runs.test(es)
+# H0: 殘差是隨機的  H1: 殘差不是隨機的
+
+#結論: 四大基本假設除了隨機性，其餘的齊一性、常態性、和函數形式皆有問題
