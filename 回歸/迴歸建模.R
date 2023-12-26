992d2140007f88570af39aed7a9920e4816c1faa
LAPTOP-U59K2599\ASUS <kyle20030819@gmail.com>
2023-12-26 23:16:01 +0800

    Phase II

--- 回歸/迴歸建模.R
+++ 回歸/迴歸建模.R
@@ -2,3 +2,9 @@
 #建立病患存活時間(Survival Time)與其他病患本身特徵間的迴歸關係,
 #以了解病患本身特徵與存活時間(Survival Time)間的關係,
 #以提供醫師及病人參考
+
+#Phase II: MAKE ASSUMPTIONS AND DEFINE ESSENTIAL VARIABLES
+#Four basic assumptions
+#All the patients' features are measured correctly.
+#Depenent Varialbe: Stime
+#Independent Variable: Other variables in the dataset.
