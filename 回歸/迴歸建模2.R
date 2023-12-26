1daa3094f7aba2738f9aca7311973361f0ead0da
LAPTOP-U59K2599\ASUS <kyle20030819@gmail.com>
2023-12-27 00:28:31 +0800

    Phase II

--- 回歸/迴歸建模2.R
+++ 回歸/迴歸建模2.R
@@ -3,3 +3,9 @@
 #以了其他影響因子與產品市場份額(Market Share)間的關係,
 #並做為後續預測產品市場份額之用,
 #以提供廠商參考
+
+#Phase II: MAKE ASSUMPTIONS AND DEFINE ESSENTIAL VARIABLES
+#Four basic assumptions
+#All the important factors are included in the study.
+#Depenent Varialbe: MShare
+#Independent Variable: Other variables in the dataset.
